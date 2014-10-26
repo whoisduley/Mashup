c6f8cb9672da542f92155b67da39c4da967e140e
whoisduley <dulelu01@luther.edu>
2014-10-26 13:15:15 -0500

    Comments

--- mashup.js
+++ mashup.js
@@ -8,12 +8,12 @@ run = function() {
 
 	  	if (request.readyState == 4)   //
 	     	if (request.status == 200) {  //successful request OK
-
+			
+			// Parses the JSON response into an object
 	        	var res = eval('(' + request.responseText + ')');
-
-	        	// var tags = res['tags'];
-	        	// alert(tags);
-
+			
+			// Takes the tags from the object and pushes them into an array.
+			// http://json.parser.online.fr/
 	        	for (var i = 0; i < 5; i++) {
 	        		var a = res.objects[0].tags[i].label;
 	        		list.push(a);
@@ -22,6 +22,7 @@ run = function() {
 	    console.log(list);
 	}
 
+	// Takes the article url from the input and glues it on the api request.
 	var urlInput = document.getElementById('urlInput');
    	var theResource = 'http://api.diffbot.com/v3/article?token=538182bfa76b57bd13b874c35a0f3f47&fields=tags&url=' + urlInput.value;
 
